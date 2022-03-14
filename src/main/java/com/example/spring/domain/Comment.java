31b219fd76ec9655ddfecd4b4fdfaea4cc6389d4
abdallah-nazzal <abdallahqawas@gmail.com>
2022-03-14 14:53:18 +0200

    Adding mapping from link->comment

--- src/main/java/com/example/spring/domain/Comment.java
+++ src/main/java/com/example/spring/domain/Comment.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 @NoArgsConstructor
@@ -17,5 +18,7 @@ public class Comment {
     private Long id;
     private String body;
 
-   // Link
+    // Link
+    @ManyToOne
+    private Link link;
 }
