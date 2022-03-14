d6ee3b787016f255c8ba285320ed888a66da43b1
abdallah-nazzal <abdallahqawas@gmail.com>
2022-03-14 13:52:19 +0200

    Created our entities and refactoed using Project Lombok

--- src/main/java/com/example/spring/Application.java
+++ src/main/java/com/example/spring/Application.java
@@ -1,14 +1,22 @@
 package com.example.spring;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
+    private static final Logger log = LoggerFactory.getLogger(SpringApplication.class);
 
+    public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+
     }
+
 
 }
