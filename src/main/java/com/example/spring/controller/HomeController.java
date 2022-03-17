5251f3dcadb5f3a8acbf0efd86a620655d36f3b8
abdallah-nazzal <abdallahqawas@gmail.com>
2022-03-17 14:47:05 +0200

    Controllers

---
+++ src/main/java/com/example/spring/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.example.spring.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home(Model model){
+        return "Hello World!";
+    }
+}
