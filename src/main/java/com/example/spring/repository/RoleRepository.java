015683c7da3baf035a3f7a4d3c81411c6f60d524
abdallah-nazzal <abdallahqawas@gmail.com>
2022-03-22 11:03:57 +0200

    Create users and roles

---
+++ src/main/java/com/example/spring/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.example.spring.repository;
+
+import com.example.spring.domain.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
