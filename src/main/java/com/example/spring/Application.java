ef5b0f809ba4bb348019df2f4c7173846994395a
abdallah-nazzal <abdallahqawas@gmail.com>
2022-04-04 12:22:48 +0300

    H2 console security configuration

--- src/main/java/com/example/spring/Application.java
+++ src/main/java/com/example/spring/Application.java
@@ -9,7 +9,6 @@ import org.slf4j.Logger;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
